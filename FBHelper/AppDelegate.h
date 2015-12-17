c9331b36ba12584395d09138a8995ca922cd5369
sergey.zhdanov <zhdanov.s.g@gmail.com>
2015-12-17 14:35:33 +0300

    Initial Commit

---
+++ FBHelper/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FBHelper
+//
+//  Created by Sergey Zhdanov on 17/12/15.
+//  Copyright © 2015 Drive Pixels Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FBHelper/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FBHelper
+//
+//  Created by Sergey Zhdanov on 17/12/15.
+//  Copyright © 2015 Drive Pixels Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
